d62552ee9db7aaaf0555326a84dc3c5476a324d2
Daedelos Ari Sopmamot-Mendoza <dev.daddyari1@gmail.com>
2018-09-23 21:50:50 -0700

    Refactor build script into multiple functions

--- buildmo.py
+++ buildmo.py
@@ -4,72 +4,78 @@
 from string import Template
 
 # global variables
+pages = [
+    {
+        "content": "content/index.html",
+        "output_file": "docs/index.html",
+        "page_title": "Home",
+        "index_active_class": " active",
+        "index_current_class": " <span class=\"sr-only\">(current)</span>",
+        "blog_active_class": "",
+        "blog_current_class": "",
+        "projects_active_class": "",
+        "projects_current_class": "",
+        "about_active_class": "",
+        "about_current_class": "",
+    },
+    {
+        "content": "content/blog.html",
+        "output_file": "docs/blog.html",
+        "page_title": "Blog",
+        "index_active_class": "",
+        "index_current_class": "",
+        "blog_active_class": " active",
+        "blog_current_class": " <span class=\"sr-only\">(current)</span>",
+        "projects_active_class": "",
+        "projects_current_class": "",
+        "about_active_class": "",
+        "about_current_class": "",
+    },
+    {
+        "content": "content/projects.html",
+        "output_file": "docs/projects.html",
+        "page_title": "Projects",
+        "index_active_class": "",
+        "index_current_class": "",
+        "blog_active_class": "",
+        "blog_current_class": "",
+        "projects_active_class": " active",
+        "projects_current_class": " <span class=\"sr-only\">(current)</span>",
+        "about_active_class": "",
+        "about_current_class": "",
+    },
+    {
+        "content": "content/about.html",
+        "output_file": "docs/about.html",
+        "page_title": "About",
+        "index_active_class": "",
+        "index_current_class": "",
+        "blog_active_class": "",
+        "blog_current_class": "",
+        "projects_active_class": "",
+        "projects_current_class": "",
+        "about_active_class": " active",
+        "about_current_class": " <span class=\"sr-only\">(current)</span>",
+    },
+]
 
 # functions
 
 def main():
     template_text = open('templates/base.html').read()
     template = Template(template_text)
+    site_pages = pages
+    bring_shrubbery(site_pages, template)
+
+def read_content(path):
+    content = open(path).read()
+    return content
     
-    pages = [
-        {
-            "content": "content/index.html",
-            "output_file": "docs/index.html",
-            "page_title": "Home",
-            "index_active_class": " active",
-            "index_current_class": " <span class=\"sr-only\">(current)</span>",
-            "blog_active_class": "",
-            "blog_current_class": "",
-            "projects_active_class": "",
-            "projects_current_class": "",
-            "about_active_class": "",
-            "about_current_class": "",
-        },
-        {
-            "content": "content/blog.html",
-            "output_file": "docs/blog.html",
-            "page_title": "Blog",
-            "index_active_class": "",
-            "index_current_class": "",
-            "blog_active_class": " active",
-            "blog_current_class": " <span class=\"sr-only\">(current)</span>",
-            "projects_active_class": "",
-            "projects_current_class": "",
-            "about_active_class": "",
-            "about_current_class": "",
-        },
-        {
-            "content": "content/projects.html",
-            "output_file": "docs/projects.html",
-            "page_title": "Projects",
-            "index_active_class": "",
-            "index_current_class": "",
-            "blog_active_class": "",
-            "blog_current_class": "",
-            "projects_active_class": " active",
-            "projects_current_class": " <span class=\"sr-only\">(current)</span>",
-            "about_active_class": "",
-            "about_current_class": "",
-        },
-        {
-            "content": "content/about.html",
-            "output_file": "docs/about.html",
-            "page_title": "About",
-            "index_active_class": "",
-            "index_current_class": "",
-            "blog_active_class": "",
-            "blog_current_class": "",
-            "projects_active_class": "",
-            "projects_current_class": "",
-            "about_active_class": " active",
-            "about_current_class": " <span class=\"sr-only\">(current)</span>",
-        },
-    ]
-    
-    # remove and replace data according to current page
-    for page in pages:
-        content = open(page['content']).read()
-        rendered_page = template.safe_substitute(
+# remove and replace data according to current page
+def bring_shrubbery(page_list, template_file):
+    for page in page_list:
+        content = read_content(page['content'])
+        rendered_page = template_file.safe_substitute(
             page_title=page['page_title'],
             index_active_class=page['index_active_class'],
             index_current_class=page['index_current_class'],
@@ -82,8 +88,6 @@ def main():
             content=content,
         )
         open(page['output_file'], 'w+').write(rendered_page)
-
-    
 
 # invoke main function
 if __name__ == "__main__":
